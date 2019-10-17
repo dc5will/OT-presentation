a8a1f04a3729041b7c0fc9005fd9fa9993597513
William Y Wong <wwong1987@gmail.com>
2019-10-17 00:40:33 -0700

    improved formatting for pdf file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
-import { pdfjs, Document, Page } from 'react-pdf';
-import spacedRep from './pdf/spacedRep.pdf';
+import React, { Component } from "react";
+import { pdfjs, Document, Page } from "react-pdf";
+import spacedRep from "./pdf/spacedRep.pdf";
+import "./App.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-
-export default class Test extends Component {
+export default class App extends Component {
   state = {
     numPages: null,
-    pageNumber: 1,
-  }
+    pageNumber: 1
+  };
 
-  onDocumentLoadSuccess = (document) => {
+  onDocumentLoadSuccess = document => {
     const { numPages } = document;
     this.setState({
       numPages,
-      pageNumber: 1,
+      pageNumber: 1
     });
   };
 
-  changePage = offset => this.setState(prevState => ({
-    pageNumber: prevState.pageNumber + offset,
-  }));
+  changePage = offset =>
+    this.setState(prevState => ({
+      pageNumber: prevState.pageNumber + offset
+    }));
 
   previousPage = () => this.changePage(-1);
 
@@ -31,33 +32,16 @@ export default class Test extends Component {
     const { numPages, pageNumber } = this.state;
 
     return (
-      <React.Fragment>
-        <Document
-          file={spacedRep}
-          onLoadSuccess={this.onDocumentLoadSuccess}
-        >
-          <Page pageNumber={pageNumber} />
-        </Document>
-        <div>
-          <p>
-            Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
-          </p>
-          <button
-            type="button"
-            disabled={pageNumber <= 1}
-            onClick={this.previousPage}
-          >
-            Previous
-          </button>
-          <button
-            type="button"
-            disabled={pageNumber >= numPages}
-            onClick={this.nextPage}
-          >
-            Next
-          </button>
-        </div>
-      </React.Fragment>
+
+      <div>
+      <Document
+        file={spacedRep}
+        onLoadSuccess={this.onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p className='pages'>Page {pageNumber} of {numPages}</p>
+    </div>
     );
   }
 }
