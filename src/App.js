3590c8bb319443a14946cce1265b3aa16a9eb226
William Y Wong <wwong1987@gmail.com>
2019-10-17 00:17:13 -0700

    fixed react-pdf to finally load test pdf

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import { pdfjs, Document, Page } from 'react-pdf';
+import spacedRep from './pdf/spacedRep.pdf';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Monica's Presentation</h1>
-      </header>
-    </div>
-  );
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+
+export default class Test extends Component {
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
+
+  onDocumentLoadSuccess = (document) => {
+    const { numPages } = document;
+    this.setState({
+      numPages,
+      pageNumber: 1,
+    });
+  };
+
+  changePage = offset => this.setState(prevState => ({
+    pageNumber: prevState.pageNumber + offset,
+  }));
+
+  previousPage = () => this.changePage(-1);
+
+  nextPage = () => this.changePage(1);
+
+  render() {
+    const { numPages, pageNumber } = this.state;
+
+    return (
+      <React.Fragment>
+        <Document
+          file={spacedRep}
+          onLoadSuccess={this.onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <div>
+          <p>
+            Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+          </p>
+          <button
+            type="button"
+            disabled={pageNumber <= 1}
+            onClick={this.previousPage}
+          >
+            Previous
+          </button>
+          <button
+            type="button"
+            disabled={pageNumber >= numPages}
+            onClick={this.nextPage}
+          >
+            Next
+          </button>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
