d4162654e555b25115c1d27ee2b7b48cef3c3c3e
William Y Wong <wwong1987@gmail.com>
2019-10-17 19:09:00 -0700

    Updates

---
+++ precache-manifest.74b2d99373ee434750f0b0b958c95a9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8366cde6acd50a8078a2349a89cba87b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "059e6754564c0296e7e4",
+    "url": "/static/css/main.0852f950.chunk.css"
+  },
+  {
+    "revision": "31eb877a6c369b31d5a8",
+    "url": "/static/js/2.c7fb79fc.chunk.js"
+  },
+  {
+    "revision": "059e6754564c0296e7e4",
+    "url": "/static/js/main.878e2ea6.chunk.js"
+  },
+  {
+    "revision": "bfdab271cb776f83ea2c",
+    "url": "/static/js/runtime-main.45413a61.js"
+  },
+  {
+    "revision": "82301cbc66b910cec6f114f374b88c07",
+    "url": "/static/media/spacedRep.82301cbc.pdf"
+  },
+  {
+    "revision": "ebbdec77b5c8aaffc51a377c60fa1966",
+    "url": "/static/media/vrlayout.ebbdec77.pdf"
+  }
+]);
